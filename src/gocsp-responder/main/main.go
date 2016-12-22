ede304daadcb8d80dbd63231c87161f38b3d1d22
Jaime Geiger <jaime@grimm-co.com>
2016-12-21 22:19:47 -0500

    get and post working. safari, chrome, and firefox are all reporting the cert as revoked

--- src/gocsp-responder/main/main.go
+++ src/gocsp-responder/main/main.go
@@ -9,6 +9,9 @@ import (
 func main() {
 	resp := responder.Responder()
 	flag.StringVar(&resp.IndexFile, "index", resp.IndexFile, "the CA index filename")
+	flag.StringVar(&resp.CaCertFile, "cacert", resp.CaCertFile, "the CA certificate filename")
+	flag.StringVar(&resp.RespCertFile, "rcert", resp.RespCertFile, "the responder certificate filename")
+	flag.StringVar(&resp.RespKeyFile, "rkey", resp.RespKeyFile, "the responder key filename")
 	flag.Parse()
 	resp.Serve()
 }
--- src/gocsp-responder/responder/responder.go
+++ src/gocsp-responder/responder/responder.go
@@ -2,61 +2,81 @@ package responder
 
 import (
 	"bufio"
+	"bytes"
+	"crypto"
+	_ "crypto/rsa"
+	"crypto/x509"
 	"encoding/base64"
-	_ "encoding/pem"
+	"encoding/pem"
 	"errors"
 	"fmt"
 	"golang.org/x/crypto/ocsp"
-	_ "io/ioutil"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 	"strconv"
 	"strings"
+	"time"
 )
 
 type OCSPResponder struct {
-	IndexFile string
-	CaKey     string
-	CaCert    string
-	Strict    bool
-	Port      int
-	Address   string
-	Ssl       bool
+	IndexFile    string
+	RespKeyFile  string //do NOT keep this in memory
+	RespCertFile string
+	CaCertFile   string
+	Strict       bool
+	Port         int
+	Address      string
+	Ssl          bool
+	CaCert       *x509.Certificate
+	RespCert     *x509.Certificate
 }
 
 func Responder() *OCSPResponder {
 	return &OCSPResponder{
-		IndexFile: "index.txt",
-		CaKey:     "ca.key",
-		CaCert:    "ca.crt",
-		Strict:    false,
-		Port:      8888,
-		Address:   "",
-		Ssl:       false,
+		IndexFile:    "index.txt",
+		RespKeyFile:  "responder.key",
+		RespCertFile: "responder.crt",
+		CaCertFile:   "ca.crt",
+		Strict:       false,
+		Port:         8888,
+		Address:      "",
+		Ssl:          false,
+		CaCert:       nil,
+		RespCert:     nil,
 	}
 }
 
 func (self *OCSPResponder) makeHandler() func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
+		log.Print("Got request")
 		if self.Strict && r.Header.Get("Content-Type") != "application/ocsp-request" {
 			fmt.Println("Strict mode requires correct Content-Type header")
 			return
 		}
-		var b []byte
+
+		b := new(bytes.Buffer)
 		switch r.Method {
 		case "POST":
-			r.Body.Read(b)
+			b.ReadFrom(r.Body)
 		case "GET":
-			b, _ = base64.StdEncoding.DecodeString(r.URL.Path[1:])
+			gd, _ := base64.StdEncoding.DecodeString(r.URL.Path[1:])
+			b.Read(gd)
 		default:
 			fmt.Println("Unsupported request method")
 			return
 		}
-		status, err := self.verify(b)
+
+		w.Header().Set("Content-Type", "application/ocsp-response")
+		resp, err := self.verify(b.Bytes())
 		if err != nil {
-			fmt.Println(err)
+			log.Print(err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
 		}
-		fmt.Println(status)
+		log.Print("Writing response...")
+		w.Write(resp)
 	}
 }
 
@@ -82,7 +102,6 @@ func (self *OCSPResponder) parseIndex() ([]IndexEntry, error) {
 		for s.Scan() {
 			var ie IndexEntry
 			ln := strings.Fields(s.Text())
-			fmt.Println(ln)
 			//probably check for error
 			ie.Status = []byte(ln[0])[0]
 			//handle strconv errors later
@@ -117,20 +136,102 @@ func (self *OCSPResponder) getIndexEntry(s uint64) (*IndexEntry, error) {
 	return nil, errors.New("Serial not found")
 }
 
-//function to get and hash CA key DN
-
 //function to get and hash the CA cert public key
-
-//takes the der encoded ocsp request and verifies it
-func (self *OCSPResponder) verify(rawreq []byte) (bool, error) {
-	req, err := ocsp.ParseRequest(rawreq)
-	ent, err := self.getIndexEntry(req.SerialNumber.Uint64())
-	fmt.Println(ent.DistinguishedName)
-	return true, err
+func parseCertFile(filename string) (*x509.Certificate, error) {
+	ct, err := ioutil.ReadFile(filename)
+	if err != nil {
+		//print out error message here
+		return nil, err
+	}
+	block, _ := pem.Decode(ct)
+	cert, err := x509.ParseCertificate(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+	return cert, nil
 }
 
-func (self *OCSPResponder) Serve() {
+func parseKeyFile(filename string) (interface{}, error) {
+	kt, err := ioutil.ReadFile(filename)
+	if err != nil {
+		//print out error message here
+		return nil, err
+	}
+	block, _ := pem.Decode(kt)
+	key, err := x509.ParsePKCS8PrivateKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+	return key, nil
+}
+
+//takes the der encoded ocsp request and verifies it
+func (self *OCSPResponder) verify(rawreq []byte) ([]byte, error) {
+	req, err := ocsp.ParseRequest(rawreq)
+	if err != nil {
+		return nil, err
+	}
+	ent, err := self.getIndexEntry(req.SerialNumber.Uint64())
+	if err != nil {
+		//cert not found status response is ocsp.Unknown
+		return nil, err
+	}
+	log.Print(ent)
+
+	var status int
+	if ent.Status == StatusRevoked {
+		status = ocsp.Revoked
+		fmt.Println("This certificate is revoked!")
+	} else {
+		status = ocsp.Good
+	}
+
+	keyi, err := parseKeyFile(self.RespKeyFile)
+	if err != nil {
+		return nil, err
+	}
+
+	key, ok := keyi.(crypto.Signer)
+	if !ok {
+		return nil, errors.New("Could not make key a signer...")
+	}
+
+	now := time.Now().Truncate(time.Minute)
+	rtemplate := ocsp.Response{
+		Status:           status,
+		SerialNumber:     req.SerialNumber,
+		Certificate:      self.RespCert,
+		RevocationReason: ocsp.Unspecified,
+		RevokedAt:        now.AddDate(0, -1, 0), //get real date later...
+		ThisUpdate:       now.AddDate(0, -2, 0),
+		NextUpdate:       now.AddDate(0, 30, 0),
+		ExtraExtensions:  nil,
+	}
+
+	resp, err := ocsp.CreateResponse(self.CaCert, self.RespCert, rtemplate, key)
+	if err != nil {
+		log.Print(err)
+		return nil, err
+	}
+
+	return resp, err
+}
+
+func (self *OCSPResponder) Serve() error {
+	//the certs should not change, so lets keep it in memory
+	cacert, err := parseCertFile(self.CaCertFile)
+	if err != nil {
+		return err
+	}
+	respcert, err := parseCertFile(self.RespCertFile)
+	if err != nil {
+		return err
+	}
+	self.CaCert = cacert
+	self.RespCert = respcert
+
 	handler := self.makeHandler()
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(fmt.Sprintf("%s:%d", self.Address, self.Port), nil)
+	return nil
 }
